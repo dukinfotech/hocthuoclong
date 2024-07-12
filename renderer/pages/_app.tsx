a48a215a3e93e5297916aabc6164e8e45991fd0f
Doan Duc <dukinfotech@gmail.com>
2024-07-12 23:01:34 +0900

    set font

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -5,26 +5,35 @@ import { NextUIProvider } from "@nextui-org/react";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
 import { ConfirmPromptProvider } from "../providers/ConfirmPromptProvider";
+import { M_PLUS_2 } from "next/font/google";
+
+const mplus2 = M_PLUS_2({
+  weight: ["400", "700"],
+  subsets: ["vietnamese"],
+  display: "swap",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <ConfirmPromptProvider>
-        <Component {...pageProps} />
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-      </ConfirmPromptProvider>
-    </NextUIProvider>
+    <main className={mplus2.className}>
+      <NextUIProvider>
+        <ConfirmPromptProvider>
+          <Component {...pageProps} />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+        </ConfirmPromptProvider>
+      </NextUIProvider>
+    </main>
   );
 }
 
