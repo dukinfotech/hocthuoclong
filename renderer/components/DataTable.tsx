4f69be00cfefb91a5677c61db4801ad6aefff413
Doan Duc <dukinfotech@gmail.com>
2024-07-16 09:40:04 +0900

    search

--- renderer/components/DataTable.tsx
+++ renderer/components/DataTable.tsx
@@ -1,5 +1,7 @@
 import {
   Checkbox,
+  Input,
+  Spacer,
   Table,
   TableBody,
   TableCell,
@@ -9,8 +11,9 @@ import {
 } from "@nextui-org/react";
 import useData from "../hooks/useData";
 import { useSettingStore } from "../stores/setting-store";
-import { useMemo } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 import { REMEMBER_FIELD } from "../const";
+import { textContentFromHTML } from "../helpers/utils";
 
 type ColumnNameType = {
   key: string;
@@ -20,6 +23,7 @@ type ColumnNameType = {
 export default function DataTable() {
   const selectedDB = useSettingStore((state) => state.selectedDB);
   const { data, update } = useData(selectedDB);
+  const [keyword, setKeyword] = useState<string>("");
 
   const columnNames = useMemo(() => {
     if (data.length > 0) {
@@ -55,46 +59,74 @@ export default function DataTable() {
     }
   }, [data]);
 
+  const filteredData = useMemo(() => {
+    return data.filter((dataObject) => {
+      let isMatched = true;
+      if (keyword) {
+        const values = Object.values(dataObject);
+        for (let i = 0; i < values.length; i++) {
+          if (i > 1) {
+            const value = values[i];
+            const plainText = textContentFromHTML(value.toString());
+            isMatched = plainText.includes(keyword);
+          }
+        }
+      }
+      return isMatched;
+    });
+  }, [data, keyword]);
+
   const toggleRemember = (key: string, e: any) => {
     console.log(key);
     update(key, REMEMBER_FIELD, e.target.checked.toString());
   };
 
   return (
-    <Table
-      hideHeader={data.length === 0}
-      isHeaderSticky
-      classNames={{
-        base: "max-h-[50vh] overflow-auto",
-      }}
-    >
-      <TableHeader>
-        {columnNames.map((columnName) => (
-          <TableColumn key={columnName.key}>{columnName.name}</TableColumn>
-        ))}
-      </TableHeader>
-      <TableBody emptyContent="Không có dữ liệu">
-        {data.map((dataObject, i) => (
-          <TableRow key={i}>
-            {columnNames.map((columnName) => (
-              <TableCell key={columnName.key}>
-                {columnName.key !== REMEMBER_FIELD ? (
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: dataObject[columnName.key],
-                    }}
-                  ></span>
-                ) : (
-                  <Checkbox
-                    defaultSelected={dataObject[columnName.key] === "true"}
-                    onChange={(e) => toggleRemember(dataObject["id"], e)}
-                  />
-                )}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <Input
+        onChange={(e) => setKeyword(e.target.value)}
+        onClear={() => setKeyword("")}
+        color="primary"
+        placeholder="Tìm kiếm"
+        className="max-w-[220px]"
+        isClearable
+      />
+      <Spacer y={2} />
+      <Table
+        hideHeader={data.length === 0}
+        isHeaderSticky
+        classNames={{
+          base: "max-h-[50vh] overflow-auto",
+        }}
+      >
+        <TableHeader>
+          {columnNames.map((columnName) => (
+            <TableColumn key={columnName.key}>{columnName.name}</TableColumn>
+          ))}
+        </TableHeader>
+        <TableBody emptyContent="Không có dữ liệu">
+          {filteredData.map((dataObject, i) => (
+            <TableRow key={i}>
+              {columnNames.map((columnName) => (
+                <TableCell key={columnName.key}>
+                  {columnName.key !== REMEMBER_FIELD ? (
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: dataObject[columnName.key],
+                      }}
+                    ></span>
+                  ) : (
+                    <Checkbox
+                      defaultSelected={dataObject[columnName.key] === "true"}
+                      onChange={(e) => toggleRemember(dataObject["id"], e)}
+                    />
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
