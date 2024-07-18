983ba2c7cb6d94571c588b225aa9532439c50712
Doan Duc <dukinfotech@gmail.com>
2024-07-18 10:29:58 +0900

    change logo file

--- renderer/const.ts
+++ renderer/const.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "Học Thuộc Lòng";
-export const APP_LOGO = "icon.ico";
+export const APP_LOGO = "logo.png";
 
 export const DATA_OBJECT_STORE_NAME = "data";
 export const REMEMBER_FIELD = "isRemembered";
