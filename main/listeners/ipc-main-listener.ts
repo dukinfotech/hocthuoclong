cc7c2a6e21d246709acbec3fd9ec3bb66b905a59
Doan Duc <dukinfotech@gmail.com>
2024-07-18 10:28:43 +0900

    enable minizable

--- main/listeners/ipc-main-listener.ts
+++ main/listeners/ipc-main-listener.ts
@@ -39,7 +39,6 @@ export default function ipcMainListener(
         alwaysOnTop: true,
         fullscreenable: false,
         maximizable: false,
-        minimizable: false,
         resizable: true,
         webPreferences: {
           preload: path.join(__dirname, "preload.js"),
